d18fd3bb148d2ce6ab455b47364aee3ebcacb65b
saurabh chaudhary <saurabh@anattadesign.com>
2014-08-14 01:47:05 +0530

    add subscribe form into footer for specific pages

--- wp-content/themes/mitm/footer.php
+++ wp-content/themes/mitm/footer.php
@@ -9,7 +9,14 @@
 ?>
 
 	</div><!-- #content -->
-
+	<?php if ( ! is_page_template( 'page-homepage.php' ) && ! is_page_template( 'page-about.php' ) ) { ?>
+		<div>
+			<form class="subscribe-form">
+				<input type="text" name="email">
+				<button type="submit">submit</button>
+			</form>
+		</div>
+	<?php } ?>
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="copy-right">
 			<?php echo get_field( 'copyright', 'option' ) ?>
